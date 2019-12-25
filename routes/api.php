22cd9e98be75030be137859cdbdb4c0947697702
novakvova <novakvova@gmail.com>
2019-12-25 13:12:06 +0200

    Add react in PHP Project

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// routes/api.php
+
+Route::get('projects', 'ProjectController@index');
+Route::post('projects', 'ProjectController@store');
+Route::get('projects/{id}', 'ProjectController@show');
+Route::put('projects/{project}', 'ProjectController@markAsCompleted');
+Route::post('tasks', 'TaskController@store');
+Route::put('tasks/{task}', 'TaskController@markAsCompleted');
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::resource('products','ProductController');
+
+Route::view('/{path?}', 'app');
